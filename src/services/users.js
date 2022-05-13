076a6891ea78d137b3ba6a12f7bada4a9c4e181b
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-05-13 14:23:13 +0200

    update

--- src/services/users.js
+++ src/services/users.js
@@ -13,3 +13,13 @@ export const usersApi = createApi({
 });
 
 export const { useGetUsersQuery } = usersApi;
+
+
+// REACT_APP_CMS_DATA=http://localhost:8080/app-data
+/* const getPages = async ({ market, environment }) => {
+  const url = process.env.REACT_APP_CMS_DATA;
+  const response = await axios.get(url, {
+    params: { market, environment },
+  });
+  return response;
+}; */
